8f39410adfb6b958144ab91470ceca8d36021028
huhuhan <869108683@qq.com>
2020-06-09 18:27:51 +0800

    update

--- demo2020/auth-security/src/main/java/com/yh/auth/security/authentication/filter/RefererCsrfFilter.java
+++ demo2020/auth-security/src/main/java/com/yh/auth/security/authentication/filter/RefererCsrfFilter.java
@@ -1,10 +1,13 @@
 package com.yh.auth.security.authentication.filter;
 
 import com.yh.auth.security.authentication.IgnoreChecker;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
  * 扩展，防止CSRF跨站请求攻击
@@ -12,8 +15,8 @@ import java.io.IOException;
  * @author yanghan
  * @date 2020/6/1
  */
+@Slf4j
 public class RefererCsrfFilter extends IgnoreChecker implements Filter {
-
 
     @Override
     public void destroy() {
@@ -24,6 +27,7 @@ public class RefererCsrfFilter extends IgnoreChecker implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
 
+        this.permitAll((HttpServletRequest) request, (HttpServletResponse) response);
         chain.doFilter(request, response);
         /*//判断是否外链。
         String referer = req.getHeader("Referer");
@@ -48,4 +52,18 @@ public class RefererCsrfFilter extends IgnoreChecker implements Filter {
     public void init(FilterConfig config) throws ServletException {
     }
 
+    private void permitAll(HttpServletRequest request, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Forward-For");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Max-Age", "1800");
+
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            log.info("报文头[" + name + "]:[" + request.getHeader(name) + "]");
+        }
+
+    }
 }
