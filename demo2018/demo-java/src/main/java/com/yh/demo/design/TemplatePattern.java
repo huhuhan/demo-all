376d25e571181efe53151a01837cab8804952981
huhuhan <869108683@qq.com>
2021-03-09 14:06:59 +0800

    demo-java：设计模式-模板模式

---
+++ demo2018/demo-java/src/main/java/com/yh/demo/design/TemplatePattern.java
@@ -0,0 +1,79 @@
+package com.yh.demo.design;
+
+/**
+ * 模板模式
+ *
+ * @author yanghan
+ * @date 2021/3/8
+ */
+public class TemplatePattern {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
+
+
+/**
+ * 抽象父类，通用方法定义逻辑执行的流程，每个接口为模板接口
+ */
+abstract class Game {
+    abstract void initialize();
+
+    abstract void startPlay();
+
+    abstract void endPlay();
+
+    /** final修饰，不可变 */
+    public final void play() {
+
+        //初始化游戏
+        initialize();
+
+        //开始游戏
+        startPlay();
+
+        //结束游戏
+        endPlay();
+    }
+}
+
+class Cricket extends Game {
+
+    @Override
+    void endPlay() {
+        System.out.println("Cricket Game Finished!");
+    }
+
+    @Override
+    void initialize() {
+        System.out.println("Cricket Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Cricket Game Started. Enjoy the game!");
+    }
+}
+
+class Football extends Game {
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+}
