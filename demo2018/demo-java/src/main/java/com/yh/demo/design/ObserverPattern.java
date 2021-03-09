1fe8691e268939b8235bc20b54938e509c72151b
huhuhan <869108683@qq.com>
2021-03-09 14:05:39 +0800

    demo-java：设计模式-观察者模式

--- demo2018/demo-java/src/main/java/com/yh/demo/design/ObserverPattern.java
+++ demo2018/demo-java/src/main/java/com/yh/demo/design/ObserverPattern.java
@@ -1,8 +1,8 @@
 package com.yh.demo.design;
 
-import com.yh.demo.design.observer.BinaryMyObserver;
-import com.yh.demo.design.observer.HexaMyObserver;
-import com.yh.demo.design.observer.MySubject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 
 /**
  * 观察者模式
@@ -32,4 +32,95 @@ public class ObserverPattern {
 
 
 
+/**
+ * 被观察者
+ * @author yanghan
+ * @date 2021/3/5
+ */
+class MySubject extends Observable {
 
+    private List<MyObserver> observers = new ArrayList<MyObserver>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllMyObservers();
+
+        // jdk内置的被观察者对象
+        setChanged();
+        super.notifyObservers();
+    }
+
+    public void attach(MyObserver observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllMyObservers() {
+        for (MyObserver observer : observers) {
+            observer.update();
+        }
+    }
+}
+
+/**
+ * 观察者接口
+ * @author yanghan
+ * @date 2021/3/5
+ */
+abstract class MyObserver {
+    MySubject subject;
+
+    abstract void update();
+}
+
+/**
+ * 观察者
+ * @author yanghan
+ * @date 2021/3/5
+ */
+class HexaMyObserver extends MyObserver implements java.util.Observer {
+
+    public HexaMyObserver(MySubject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("java.util.Observer|Hex: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
+
+
+/**
+ * 观察者
+ *
+ * @author yanghan
+ * @date 2021/3/5
+ */
+class BinaryMyObserver extends MyObserver implements java.util.Observer {
+
+    public BinaryMyObserver(MySubject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Binary: " + Integer.toBinaryString(subject.getState()));
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("java.util.Observer|Binary: " + Integer.toOctalString(subject.getState()));
+    }
+}
