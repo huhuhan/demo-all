49e243e8c9226ab79ac757a61a14d468868dd188
huhuhan <869108683@qq.com>
2021-03-18 15:20:48 +0800

    demo-java：设计模式-解释器模式

---
+++ demo2018/demo-java/src/main/java/com/yh/demo/design/InterpreterPattern.java
@@ -0,0 +1,96 @@
+package com.yh.demo.design;
+
+/**
+ * 解释器模式
+ * @author yanghan
+ * @date 2021/3/12
+ */
+public class InterpreterPattern {
+
+    //规则：Robert 和 John 是男性
+    public static Expression getMaleExpression(){
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    //规则：Julie 是一个已婚的女性
+    public static Expression getMarriedWomanExpression(){
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("XXX is male? " + isMale.interpret("XXX"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+    }
+}
+
+/**
+ * 解释器-表达式接口
+ */
+interface Expression {
+    boolean interpret(String context);
+}
+
+/**
+ * 包含-表达式
+ */
+class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data){
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * 或者-表达式
+ */
+class OrExpression implements Expression {
+
+    private Expression expr1;
+    private Expression expr2;
+
+    public OrExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) || expr2.interpret(context);
+    }
+}
+
+/**
+ * 并且-表达式
+ */
+class AndExpression implements Expression {
+
+    private Expression expr1;
+    private Expression expr2;
+
+    public AndExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) && expr2.interpret(context);
+    }
+}
--- demo2018/demo-java/src/main/java/com/yh/demo/design/SingletonPattern.java
+++ demo2018/demo-java/src/main/java/com/yh/demo/design/SingletonPattern.java
@@ -8,16 +8,9 @@ package com.yh.demo.design;
  */
 public class SingletonPattern {
     public static void main(String[] args) {
-
-        //不合法的构造函数
-        //编译时错误：构造函数 SingleObject() 是不可见的
-//        SingleObject object = new SingleObject();
-
-        //获取唯一可用的对象
-        SingleObject object = SingleObject.getInstance();
-        SingleObject object2 = SingleObject.getInstance();
-
-        System.out.println(object == object2);
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> System.out.println(Singleton3.getInstance())).start();
+        }
     }
 }
 
@@ -75,9 +68,12 @@ class Singleton2 {
  * jvm装载类时，单线程操作，线程安全
  */
 class Singleton3 {
-    /** 没有懒加载效果，即JVM装载类直接初始化，和内存有关（类属性都不推荐直接给初始值，尤其POJO类）**/
+    /** 没有懒加载效果，即JVM装载类直接初始化，和内存有关（类属性都不推荐直接给初始值，尤其POJO类） **/
     private static Singleton3 instance = new Singleton3();
-    private Singleton3 (){}
+
+    private Singleton3() {
+    }
+
     public static Singleton3 getInstance() {
         return instance;
     }
@@ -88,7 +84,10 @@ class Singleton3 {
  */
 class Singleton4 {
     private volatile static Singleton4 singleton;
-    private Singleton4 (){}
+
+    private Singleton4() {
+    }
+
     public static Singleton4 getSingleton() {
         if (singleton == null) {
             synchronized (Singleton4.class) {
@@ -108,7 +107,10 @@ class Singleton5 {
     private static class SingletonHolder {
         private static final Singleton5 INSTANCE = new Singleton5();
     }
-    private Singleton5 (){}
+
+    private Singleton5() {
+    }
+
     public static Singleton5 getInstance() {
         return SingletonHolder.INSTANCE;
     }
